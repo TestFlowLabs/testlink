b8446afc5f6501af862193a209db99ff7a6a93be
Yunus Emre Deligöz <ye@deligoz.me>
2025-12-03 14:45:03 +0300

    build: configure package as testflowlabs/testlink

--- rector.php
+++ rector.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
